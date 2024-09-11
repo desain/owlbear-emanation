cde181975b57f75dfc367e0a0d901c43cac99d50
Nicholas Desai <desainic@amazon.com>
2024-09-11 18:05:49 -0500

    Factor out update

--- src/background.ts
+++ src/background.ts
@@ -32,10 +32,30 @@ OBR.onReady(() => {
   });
 
   const emanationReplaceLock = new AwaitLock();
-  OBR.scene.items.onChange(async (items) => {
-    // TODO mutex?
-    // https://yarnpkg.com/package?q=mutex&name=await-lock
+  OBR.scene.items.onChange((items) => updateEmanations(emanationReplaceLock, items, ({metadata, sourceItem}) => {
+    const newScale = sourceItem.scale;
+    return !Math2.compare(newScale, metadata.sourceScale, 0.01);
+  }));
+  
+  OBR.scene.grid.onChange(async (_) => updateEmanations(emanationReplaceLock, null, _ => true));
+});
+
+function getStyle(emanation: Item): EmanationStyle {
+  if (isCurve(emanation)) {
+    return emanation.style;
+  } else if (isShape(emanation)) {
+    return emanation.style;
+  } else {
+    const metadata = emanation.metadata[getPluginId("metadata")] as EmanationMetadata;
+    return metadata.style;
+  }
+}
+
+async function updateEmanations(emanationReplaceLock: AwaitLock, items: Item[] | null, updateFilter: (_: {metadata: EmanationMetadata, sourceItem: Item}) => boolean) {
     await emanationReplaceLock.acquireAsync();
+    if (!items) {
+      items = await OBR.scene.items.getItems();
+    }
     try {
       const emanationsToUpdate = items.filter(isEmanation)
         .map((emanation) => {
@@ -51,10 +71,7 @@ OBR.onReady(() => {
           };
         })
         .filter(x => x !== null)
-        .filter(({metadata, sourceItem}) => {
-          const newScale = sourceItem.scale;
-          return !Math2.compare(newScale, metadata.sourceScale, 0.01);
-        });
+        .filter(updateFilter);
       if (emanationsToUpdate.length === 0) {
         return;
       }
@@ -76,69 +93,4 @@ OBR.onReady(() => {
     } finally {
       emanationReplaceLock.release();
     }
-  });
-  
-  OBR.scene.grid.onChange(async (grid) => {
-    await emanationReplaceLock.acquireAsync();
-    try {
-      console.log('grid change');
-      console.log('start with emanations', (await OBR.scene.items.getItems(isEmanation)).map(x => x.id));
-  
-      const items = await OBR.scene.items.getItems();
-      const emanationsToUpdate = items.filter(isEmanation)
-        .map((emanation) => {
-          const sourceItem = items.find((item) => item.id === emanation.attachedTo);
-          if (!sourceItem || !isImage(sourceItem)) {
-            return null;
-          }
-          return {
-            id: emanation.id,
-            style: getStyle(emanation),
-            metadata: emanation.metadata[getPluginId("metadata")] as EmanationMetadata,
-            sourceItem
-          };
-        })
-        .filter((x) => x !== null);
-      if (emanationsToUpdate.length === 0) {
-        return;
-      }
-      console.log(`creating replacements for ${emanationsToUpdate.length} emanations`)
-  
-      const gridDpi = grid.dpi;
-      const gridMultiplier = (await OBR.scene.grid.getScale()).parsed.multiplier;
-      const gridMeasurement = grid.measurement;
-      const gridType = grid.type;
-      const replacements = emanationsToUpdate.map(({style, metadata, sourceItem}) => buildEmanation(
-        sourceItem,
-        style,
-        metadata.size,
-        gridDpi,
-        gridMultiplier,
-        gridMeasurement,
-        gridType,
-      ));
-      console.log('now have', (await OBR.scene.items.getItems(isEmanation)).map(x => x.id));
-  
-      console.log(`deleting ${emanationsToUpdate.length} emanations`)
-      await OBR.scene.items.deleteItems(emanationsToUpdate.map(({id}) => id));
-      console.log('now have', (await OBR.scene.items.getItems(isEmanation)).map(x => x.id));
-      console.log(`adding ${replacements.length} replacements`)
-      await OBR.scene.items.addItems(replacements);
-    } finally {
-      emanationReplaceLock.release();
-    }
-
-    
-  });
-});
-
-function getStyle(emanation: Item): EmanationStyle {
-  if (isCurve(emanation)) {
-    return emanation.style;
-  } else if (isShape(emanation)) {
-    return emanation.style;
-  } else {
-    const metadata = emanation.metadata[getPluginId("metadata")] as EmanationMetadata;
-    return metadata.style;
-  }
 }
