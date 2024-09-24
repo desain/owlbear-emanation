bd2d533492474dbf84e959746e5cc67b47b6dee0
Nicholas Desai <desainic@amazon.com>
2024-09-24 01:15:15 -0500

    Fix multiple load

--- background.ts
+++ background.ts
@@ -1,22 +1,6 @@
-import OBR from "@owlbear-rodeo/sdk";
 import installDragTool from "./dragtool/src/install";
 import installEmanations from "./emanation/src/install";
-
-OBR.onReady(async () => {
-    let uninstallAll: (() => void) | null = null;
-    if (await OBR.scene.isReady()) {
-        uninstallAll = await install();
-    }
-    OBR.scene.onReadyChange(async (ready) => {
-        if (ready) {
-            uninstallAll = await install();
-        } else {
-            if (uninstallAll) {
-                uninstallAll();
-            }
-        }
-    });
-});
+import ready from "./ready";
 
 async function install() {
     const uninstallers = await Promise.all([
@@ -25,3 +9,5 @@ async function install() {
     ]);
     return () => uninstallers.forEach(uninstaller => uninstaller());
 }
+
+ready(install);
