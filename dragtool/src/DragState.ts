418f03f14230d9c2e51508d5c87603a97afe60fb
Nicholas Desai <nicholassdesai@gmail.com>
2024-10-10 23:07:07 -0500

    Update elements to collision point on collision

--- dragtool/src/DragState.ts
+++ dragtool/src/DragState.ts
@@ -1,6 +1,6 @@
-import OBR, { Item, Math2, Vector2 } from "@owlbear-rodeo/sdk";
+import OBR, { GridScale, Item, Math2, Vector2 } from "@owlbear-rodeo/sdk";
 import { AbstractInteraction, createLocalInteraction, wrapRealInteraction } from "./AbstractInteraction";
-import { METADATA_KEY } from "./constants";
+import { METADATA_KEY, VECTOR2_COMPARE_EPSILON } from "./constants";
 import { createDragMarker } from "./Sequence/DragMarker";
 import { assertHasMetadata } from "./Sequence/metadataUtils";
 import { Segment, createSegment, getSegmentText } from "./Sequence/Segment";
@@ -11,9 +11,16 @@ import { Waypoint, createWaypoint } from "./Sequence/Waypoint";
 import { WaypointLabel, createWaypointLabel, getWaypointLabelText } from "./Sequence/WaypointLabel";
 import Snapper from "./Snapper";
 
+type DragInteractionItems = {
+    target: SequenceTarget,
+    sweeps: Sweep[],
+    segment: Segment,
+    waypoint: Waypoint,
+    waypointLabel: WaypointLabel,
+}
+
 export default class DragState {
     private readonly start: Vector2;
-    private end: Vector2;
     private readonly sweepData: SweepData[];
     private readonly interaction: AbstractInteraction<Item[]>;
     private readonly snapper: Snapper;
@@ -27,7 +34,25 @@ export default class DragState {
      */
     private readonly targetIsNew: boolean;
 
-    static setupOrCreateTarget(
+    private constructor(
+        start: Vector2,
+        baseDistance: number,
+        distanceScaling: number,
+        interaction: AbstractInteraction<Item[]>,
+        sweepData: SweepData[],
+        snapper: Snapper,
+        targetIsNew: boolean,
+    ) {
+        this.start = start;
+        this.baseDistance = baseDistance;
+        this.distanceScaling = distanceScaling;
+        this.interaction = interaction;
+        this.snapper = snapper;
+        this.targetIsNew = targetIsNew;
+        this.sweepData = sweepData;
+    }
+
+    private static setupOrCreateTarget(
         targetArg: Item | null,
         positionForNew: Vector2,
         dpi: number,
@@ -74,20 +99,19 @@ export default class DragState {
 
         const snapper = new Snapper(targetArg, measurement, gridType);
         const layer = aboveCharacters ? 'RULER' : 'DRAWING';
-        const end = await snapper.snap(pointerPosition);
+        const [end] = await snapper.snap(pointerPosition);
         let { target, targetIsNew } = DragState.setupOrCreateTarget(targetArg, end, dpi, playerColor, privateMode);
         const { sweeps, sweepDatas: sweepData } = await getSweeps(target);
-        const ruler = createSegment(target, end, layer, distanceScaling);
+        const segment = createSegment(target, end, layer, distanceScaling);
         const waypoint = createWaypoint(target, layer, dpi, playerColor);
         const waypointLabel = createWaypointLabel(target);
-        const interactionItems = DragState.composeItems({ target, sweeps, ruler, waypointLabel, waypoint });
+        const interactionItems = DragState.composeItems({ target, sweeps, segment, waypointLabel, waypoint });
         const interaction: AbstractInteraction<Item[]> = privateMode
             ? await createLocalInteraction(interactionItems)
             : await wrapRealInteraction(interactionItems);
 
         return new DragState(
             target.position,
-            end,
             await getSequenceLength(target.id, interaction.itemApi),
             distanceScaling,
             interaction,
@@ -97,123 +121,121 @@ export default class DragState {
         );
     }
 
-    private constructor(
-        start: Vector2,
-        end: Vector2,
-        baseDistance: number,
-        distanceScaling: number,
-        interaction: AbstractInteraction<Item[]>,
-        sweepData: SweepData[],
-        snapper: Snapper,
-        targetIsNew: boolean,
-    ) {
-        this.start = start;
-        this.end = end;
-        this.baseDistance = baseDistance;
-        this.distanceScaling = distanceScaling;
-        this.interaction = interaction;
-        this.snapper = snapper;
-        this.targetIsNew = targetIsNew;
-        this.sweepData = sweepData;
-    }
-
-    /**
-     * Set endpoint, snapping to grid.
-     * @param end Non-snapped endpoint
-     * @returns Whether the endpoint changed.
-     */
-    private async setEnd(end: Vector2) {
-        const oldEnd = this.end;
-        this.end = await this.snapper.snap(end);
-        return oldEnd.x != this.end.x || oldEnd.y != this.end.y;
-    }
-
-    private static composeItems({ target, sweeps, ruler, waypointLabel, waypoint }: ReturnType<typeof DragState.prototype.decomposeItems>): Item[] {
-        // For some reason OBR wants existing items first in the interaction array, then newly created ones.
-        // It doesn't display updates to the existing items if existing items are at the back.
-        return [target, ...sweeps, ruler, waypointLabel, waypoint];
-    }
-
     /**
      * Break down the misc items in an interaction.
      * @param items Misc items
      * @returns object that labels items
      */
-    private decomposeItems(items: Item[]) {
+    private decomposeItems(items: Item[]): DragInteractionItems {
         let idx = 0;
-        const target = items[idx++] as SequenceTarget;
+        const target: SequenceTarget = assertHasMetadata(items[idx++]);
 
         const numSweeps = this.sweepData.length;
         const sweeps = items.slice(idx, idx += numSweeps) as Sweep[];
 
-        const ruler = items[idx++] as Segment;
+        const segment = items[idx++] as Segment;
         const waypointLabel = items[idx++] as WaypointLabel;
         const waypoint = items[idx++] as Waypoint;
-        return { target, sweeps, ruler, waypointLabel, waypoint };
+        return { target, sweeps, segment, waypointLabel, waypoint };
+    }
+
+
+    private static composeItems({ target, sweeps, segment, waypointLabel, waypoint }: DragInteractionItems): Item[] {
+        // For some reason OBR wants existing items first in the interaction array, then newly created ones.
+        // It doesn't display updates to the existing items if existing items are at the back.
+        return [
+            // existing items
+            target, ...sweeps,
+            // new items
+            segment, waypointLabel, waypoint
+        ];
+    }
+
+    private async getDistanceAndScale(end: Vector2) {
+        const [unadjustedDistance, scale] = await Promise.all([
+            OBR.scene.grid.getDistance(this.start, end),
+            OBR.scene.grid.getScale(),
+        ]);
+        return { unadjustedDistance, scale };
+    }
+
+    private updateItems(
+        changedEnd: boolean,
+        end: Vector2,
+        unadjustedDistance: number,
+        scale: GridScale,
+        { target, sweeps, segment, waypointLabel }: DragInteractionItems,
+    ) {
+        if (changedEnd) {
+            const adjustedDistance = unadjustedDistance * this.distanceScaling;
+            const totalDistance = this.baseDistance + adjustedDistance;
+            waypointLabel.text = { ...waypointLabel.text, plainText: getWaypointLabelText(totalDistance, scale) };
+            waypointLabel.position = end;
+
+            segment.endPosition = end;
+            segment.measurement = getSegmentText(unadjustedDistance, scale, this.distanceScaling);
+
+            target.position = end;
+
+            const movementVector = Math2.subtract(end, this.start);
+            for (let i = 0; i < sweeps.length; i++) {
+                const sweep = sweeps[i];
+                const startPosition = Math2.add(this.start, this.sweepData[i].baseOffset);
+                const sweepCommands = this.sweepData[i].sweeper(startPosition, movementVector);
+                sweep.commands = [...this.sweepData[i].baseCommands, ...sweepCommands];
+            }
+        }
     }
 
     async update(pointerPosition: Vector2) {
-        const changedEnd = await this.setEnd(pointerPosition);
-        const [unadjustedNewDistance, scale] = await Promise.all([
-            OBR.scene.grid.getDistance(this.start, this.end),
-            OBR.scene.grid.getScale(),
-        ]);
-        const adjustedNewDistance = unadjustedNewDistance * this.distanceScaling;
-        const totalDistance = this.baseDistance + adjustedNewDistance;
+        const [end, changedEnd] = await this.snapper.snap(pointerPosition);
+        const { unadjustedDistance, scale } = await this.getDistanceAndScale(end);
         const { update } = this.interaction;
         const items = await update((items) => {
-            if (changedEnd) {
-                const { target, sweeps, ruler, waypointLabel } = this.decomposeItems(items);
-                waypointLabel.text = { ...waypointLabel.text, plainText: getWaypointLabelText(totalDistance, scale) };
-                waypointLabel.position = this.end;
-
-                ruler.endPosition = this.end;
-                ruler.measurement = getSegmentText(unadjustedNewDistance, scale, this.distanceScaling);
-
-                target.position = this.end;
-
-                const movementVector = Math2.subtract(this.end, this.start);
-                for (let i = 0; i < sweeps.length; i++) {
-                    const sweep = sweeps[i];
-                    const startPosition = Math2.add(this.start, this.sweepData[i].baseOffset);
-                    const sweepCommands = this.sweepData[i].sweeper(startPosition, movementVector);
-                    sweep.commands = [...this.sweepData[i].baseCommands, ...sweepCommands];
-                }
-            }
+            this.updateItems(changedEnd, end, unadjustedDistance, scale, this.decomposeItems(items));
         });
-        return this.decomposeItems(items);
+
+        return { ...this.decomposeItems(items), end };
     }
 
     async finish(pointerPosition: Vector2) {
-        if (this.start.x === this.end.x && this.start.y === this.end.y) {
-            await this.cancel();
-            return;
+        const { target, segment, waypointLabel, sweeps, waypoint, end } = await this.update(pointerPosition);
+
+        if (this.start.x === end.x && this.start.y === end.y) {
+            return await this.cancel();
         }
 
-        const { target, ruler, waypointLabel, sweeps, waypoint } = await this.update(pointerPosition);
         const { keepAndStop, itemApi } = this.interaction;
 
-        const toAdd: Item[] = [ruler, waypointLabel, waypoint, ...sweeps];
+        const toAdd: Item[] = [segment, waypointLabel, ...sweeps, waypoint];
         if (this.targetIsNew) {
             toAdd.push(target);
         } else {
-            // Network item values snap back by default so update it for real
-            // Do this before stopping the interaction to avoid visual flicker
+            // Network item values snap back by default when an interaction is stopped,
+            // so finalize the interaction by updating the target for real.
+            // Do this before stopping the interaction to avoid visual flicker.
+            // Also, this may cause wall collision.
             await itemApi.updateItems([target.id], ([realTarget]) => {
                 realTarget.position = target.position;
                 realTarget.metadata[METADATA_KEY] = target.metadata[METADATA_KEY];
                 // This triggers an update which checks if the target has moved outside its sequence,
                 // so don't mark it as done dragging yet so we skip that check, since the sequence
-                // segment for this move doesn't exist yet for network drags
+                // segment for this move doesn't exist yet for network drags.
             });
         }
 
         await keepAndStop(toAdd);
 
-        // Now that the sequence segment definitely exists, we can mark the item as not being dragged
-        await itemApi.updateItems([target], ([target]) => {
-            let sequenceItemTarget: SequenceTarget = assertHasMetadata(target);
-            sequenceItemTarget.metadata[METADATA_KEY].activelyDragging = false;
+        // but now collision might have been applied, so fix the segment and label
+        const [collidedTarget] = await itemApi.getItems([target.id]);
+        const changedEnd = !Math2.compare(target.position, collidedTarget.position, VECTOR2_COMPARE_EPSILON)
+        const { unadjustedDistance, scale } = await this.getDistanceAndScale(collidedTarget.position);
+        await itemApi.updateItems(DragState.composeItems({ target, sweeps, segment, waypointLabel, waypoint }), (items) => {
+            const dragInteractionItems = this.decomposeItems(items);
+            this.updateItems(changedEnd, collidedTarget.position, unadjustedDistance, scale, dragInteractionItems);
+
+            // Now that the sequence segment definitely exists, we can mark the item as not being dragged
+            dragInteractionItems.target.metadata[METADATA_KEY].activelyDragging = false;
         });
     }
 
