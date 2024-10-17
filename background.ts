ae5d9421e663d94001b84735f489774763045dc2
Nicholas Desai <nicholassdesai@gmail.com>
2024-10-17 02:10:55 -0500

    Split out dragtool

--- background.ts
+++ background.ts
@@ -1,11 +1,9 @@
-import installDragTool from "./dragtool/src/install";
 import installEmanations from "./emanation/src/install";
 import ready from "./ready";
 
 async function install() {
     const uninstallers = await Promise.all([
         installEmanations(),
-        installDragTool(),
     ]);
     return () => uninstallers.forEach(uninstaller => uninstaller());
 }
