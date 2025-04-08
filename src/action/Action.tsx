f3c369c3169e3308aae5d5688a06fd6f7507acca
Nicholas Desai <nicholassdesai@gmail.com>
2025-04-07 20:56:02 -0500

    Add undo and redo

--- src/action/Action.tsx
+++ src/action/Action.tsx
@@ -13,35 +13,7 @@ import { SceneSettingsTab } from "./SettingsTab";
 const BASE_HEIGHT = 100;
 const MAX_HEIGHT = 700;
 
-function TabContent({
-    index,
-    currentIndex,
-    children,
-}: {
-    index: number;
-    currentIndex: number;
-    children: React.ReactNode;
-}) {
-    if (index === currentIndex) {
-        return children;
-    }
-    return null;
-}
-
-export function Action() {
-    const [currentTab, setCurrentTab] = useState(0);
-    const role = useOwlbearStore((store) => store.role);
-
-    useEffect(() => {
-        return OBR.broadcast.onMessage(TAB_CHANNEL, ({ data }) => {
-            if (typeof data === "number") {
-                setCurrentTab(data);
-            }
-        });
-    });
-
-    const tabContainer = useRef(null);
-
+function useActionResizer(tabContainer: React.RefObject<HTMLElement>) {
     useEffect(() => {
         if (!tabContainer.current) {
             return;
@@ -71,6 +43,67 @@ export function Action() {
             void OBR.action.setHeight(BASE_HEIGHT);
         };
     }, []);
+}
+
+/**
+ * When a common key is pressed ensure the action is performed in OBR
+ * This is done because the OBR window might not have focus so the
+ * key won't be triggered
+ * Adapted from https://github.com/owlbear-rodeo/outliner/blob/main/src/Outliner.tsx#L42
+ */
+function useUndoRedoHandler() {
+    useEffect(() => {
+        async function handleKeyDown(e: KeyboardEvent) {
+            if (e.key === "z" && (e.ctrlKey || e.metaKey)) {
+                e.preventDefault();
+                e.stopPropagation();
+                if (e.shiftKey) {
+                    await OBR.scene.history.redo();
+                } else {
+                    await OBR.scene.history.undo();
+                }
+            }
+        }
+
+        document.addEventListener("keydown", handleKeyDown);
+
+        return () => {
+            document.removeEventListener("keydown", handleKeyDown);
+        };
+    }, []);
+}
+
+function TabContent({
+    index,
+    currentIndex,
+    children,
+}: {
+    index: number;
+    currentIndex: number;
+    children: React.ReactNode;
+}) {
+    if (index === currentIndex) {
+        return children;
+    }
+    return null;
+}
+
+export function Action() {
+    const [currentTab, setCurrentTab] = useState(0);
+    const role = useOwlbearStore((store) => store.role);
+
+    useEffect(() => {
+        return OBR.broadcast.onMessage(TAB_CHANNEL, ({ data }) => {
+            if (typeof data === "number") {
+                setCurrentTab(data);
+            }
+        });
+    });
+
+    const tabContainer = useRef(null);
+    useActionResizer(tabContainer);
+
+    useUndoRedoHandler();
 
     return (
         <>
