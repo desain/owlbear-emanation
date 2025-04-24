2a9923a7c0ddcdc417ddcbb8bcd1b937c1b69ca4
Nicholas Desai <desainic@amazon.com>
2025-04-23 23:57:36 -0500

    Add dropdown to new aura button

---
+++ src/action/NewAuraButton.tsx
@@ -0,0 +1,117 @@
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import {
+    Button,
+    ButtonGroup,
+    ClickAwayListener,
+    Grow,
+    MenuItem,
+    MenuList,
+    Paper,
+    Popper,
+} from "@mui/material";
+import { Item } from "@owlbear-rodeo/sdk";
+import { useRef, useState } from "react";
+import { usePlayerStorage } from "../state/usePlayerStorage";
+import { isCandidateSource } from "../types/CandidateSource";
+import { createAuras, createAurasWithDefaults } from "../utils/createAuras";
+
+interface NewAuraButtonProps {
+    disabled: boolean;
+    targetedItems: Item[];
+}
+
+export function NewAuraButton({ disabled, targetedItems }: NewAuraButtonProps) {
+    const [open, setOpen] = useState(false);
+    const anchorRef = useRef<HTMLDivElement>(null);
+    const presets = usePlayerStorage((store) => store.presets);
+
+    const handleClose = (event: Event) => {
+        if (
+            anchorRef.current &&
+            anchorRef.current.contains(event.target as HTMLElement)
+        ) {
+            return;
+        }
+
+        setOpen(false);
+    };
+
+    return (
+        <>
+            <ButtonGroup
+                variant="outlined"
+                ref={anchorRef}
+                aria-label="split button"
+                disabled={disabled}
+            >
+                <Button
+                    startIcon={<AddCircleIcon />}
+                    onClick={() =>
+                        createAurasWithDefaults(
+                            targetedItems.filter(isCandidateSource),
+                        )
+                    }
+                >
+                    New
+                </Button>
+                <Button
+                    size="small"
+                    aria-controls={open ? "split-button-menu" : undefined}
+                    aria-expanded={open ? "true" : undefined}
+                    aria-haspopup="menu"
+                    onClick={() => {
+                        setOpen((prevOpen) => !prevOpen);
+                    }}
+                >
+                    <ArrowDropDownIcon />
+                </Button>
+            </ButtonGroup>
+            <Popper
+                sx={{
+                    zIndex: 1,
+                }}
+                open={open}
+                anchorEl={anchorRef.current}
+                role={undefined}
+                transition
+                disablePortal
+            >
+                {({ TransitionProps, placement }) => (
+                    <Grow
+                        {...TransitionProps}
+                        style={{
+                            transformOrigin:
+                                placement === "bottom"
+                                    ? "center top"
+                                    : "center bottom",
+                        }}
+                    >
+                        <Paper>
+                            <ClickAwayListener onClickAway={handleClose}>
+                                <MenuList id="split-button-menu" autoFocusItem>
+                                    {presets.map((preset) => (
+                                        <MenuItem
+                                            key={preset.id}
+                                            onClick={() => {
+                                                setOpen(false);
+                                                return createAuras(
+                                                    targetedItems.filter(
+                                                        isCandidateSource,
+                                                    ),
+                                                    preset.config,
+                                                );
+                                            }}
+                                        >
+                                            {preset.name}
+                                        </MenuItem>
+                                    ))}
+                                </MenuList>
+                            </ClickAwayListener>
+                        </Paper>
+                    </Grow>
+                )}
+            </Popper>
+        </>
+    );
+}
