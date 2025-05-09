b1f4f5e1b7b536df6856bc5d936d026291fcf6a3
Nicholas Desai <nicholassdesai@gmail.com>
2025-05-08 23:36:27 -0500

    use axono utils

--- src/utils/axonometricUtils.ts
+++ src/utils/axonometricUtils.ts
@@ -1,15 +1,7 @@
-import type { GridType, Matrix, Vector2 } from "@owlbear-rodeo/sdk";
+import type { GridType, Matrix } from "@owlbear-rodeo/sdk";
 import { MathM } from "@owlbear-rodeo/sdk";
-import { ANGLE_DIMETRIC_RADIANS, PI_6 } from "owlbear-utils";
+import { SCALE_DIMETRIC, SCALE_ISOMETRIC } from "owlbear-utils";
 
-export const SCALE_ISOMETRIC: Vector2 = {
-    x: Math.SQRT1_2 / Math.tan(PI_6),
-    y: Math.SQRT1_2,
-};
-export const SCALE_DIMETRIC: Vector2 = {
-    x: Math.SQRT1_2 / Math.tan(ANGLE_DIMETRIC_RADIANS),
-    y: Math.SQRT1_2,
-};
 const ROTATE45 = MathM.fromRotation(45);
 export const TRANSFORM_ISOMETRIC = MathM.multiply(
     MathM.fromScale(SCALE_ISOMETRIC),
@@ -31,15 +23,5 @@ export function getAxonometricTransformMatrix(
         return TRANSFORM_DIMETRIC;
     } else {
         return null;
-    }
-}
-
-export function getScale(gridType: GridType): Vector2 {
-    if (gridType === "ISOMETRIC") {
-        return SCALE_ISOMETRIC;
-    } else if (gridType === "DIMETRIC") {
-        return SCALE_DIMETRIC;
-    } else {
-        return { x: 1, y: 1 };
     }
 }
