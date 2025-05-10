4536d6d5d10a7c4aede08515e6342467d023a268
Nicholas Desai <desainic@amazon.com>
2025-05-10 16:55:39 -0500

    Add shift mode to store

--- src/constants.ts
+++ src/constants.ts
@@ -4,15 +4,18 @@ const PLUGIN_ID = "com.desain.emanation";
 export const METADATA_KEY = `${PLUGIN_ID}/metadata`;
 
 // Context Menu
-export const ID_CONTEXTMENU_CREATE = `${PLUGIN_ID}/contextmenu-create`;
-export const ID_CONTEXTMENU_EDIT = `${PLUGIN_ID}/contextmenu-edit`;
+export const ID_CONTEXTMENU_CREATE = `${PLUGIN_ID}/contextMenuCreate`;
+export const ID_CONTEXTMENU_EDIT = `${PLUGIN_ID}/contextMenuEdit`;
 
 // Broadcasts
 export const CHANNEL_MESSAGE = `${PLUGIN_ID}/message`;
-export const CHANNEL_TAB = `${PLUGIN_ID}/action-set-tab`;
+export const CHANNEL_TAB = `${PLUGIN_ID}/actionSetTab`;
 
 // Store
 export const PLAYER_SETTINGS_STORE_NAME = `${PLUGIN_ID}/playerSettings`;
 
 // Misc
 export const VECTOR2_COMPARE_EPSILON = 0.01;
+
+// Tool
+export const ID_TOOL_MODE_SHIFT_AURA = `${PLUGIN_ID}/toolModeShitAura`;
