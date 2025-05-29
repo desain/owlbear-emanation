7403dc5a3a99620b42ccd1c0a5ff8260e2a60e48
Nicholas Desai <nicholassdesai@gmail.com>
2025-05-29 17:45:58 -0500

    add mkcert

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
+// import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), glsl()],
+    plugins: [react(), glsl(), mkcert()],
     server: {
         cors: true,
+        // https: true,
     },
     build: {
         assetsInlineLimit: 0, // disable inlining assets since that doesn't work for OBR
